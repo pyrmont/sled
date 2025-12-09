572d7fc057778236083025ddddb36017a4f47691
Michael Camilleri <mike@inqk.net>
2025-12-09 15:18:23 +0900

    Add support for Linux to Homebrew formula

--- sled.rb
+++ sled.rb
@@ -10,6 +10,18 @@ class Sled < Formula
     end
   end
 
+  on_linux do
+    on_arm do
+      url "https://github.com/pyrmont/sled/releases/download/v#{version}/sled-v#{version}-linux-aarch64.tar.gz"
+      sha256 "e44615061b447443b400d56e7b66f184673c1fe2a1d87a4d3f226c8176e46024"
+    end
+
+    on_intel do
+      url "https://github.com/pyrmont/sled/releases/download/v#{version}/sled-v#{version}-linux-x86_64.tar.gz"
+      sha256 "e6860c649e3499eae321b48562d5b8e8e4caeab8e722e766411de46749f5a533"
+    end
+  end
+
   def install
     bin.install "sled"
     man1.install "sled.1"
